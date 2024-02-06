1ee74bd0447a13c3177ef45ebd61dc3664a62121
HuyMasterTX <huymastertx88@gmail.com>
2024-02-06 09:56:48 +0700

    Function complete

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Collections.Immutable;
 using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
@@ -15,9 +16,18 @@ class Program {
     static void Function() {
         string username = GetUsername(0);
         CustomerType customer = GetCustomerType(2);
-        double lastMonth = GetWaterUse(4, "Amount of water used last month: ");
-        double thisMonth = GetWaterUse(5, "Amount of water used last month: ");
+        double lastMonth = GetWaterUse(4, "Amount of water used last month: ", "Last month: ");
+        double thisMonth = GetWaterUse(5, "Amount of water used this month: ", "This month: ");
 
+        double lastMonthFee = customer.GetCalcMethod().Invoke(lastMonth);
+        double thisMonthFee = customer.GetCalcMethod().Invoke(thisMonth);
+        bool useMore = thisMonthFee > lastMonthFee;
+
+        ConsoleUtils.WriteWithColorAt(7,
+            FormatText.Of(String.Format("Last month: {0,11:0,0} VND", lastMonthFee), null));
+        ConsoleUtils.WriteWithColorAt(8,
+            FormatText.Of(String.Format("This month: {0,11:0,0} VND", thisMonthFee), null),
+            FormatText.Of(String.Format(" ({0}{1:0,0} VND)", useMore ? "+" : "", thisMonthFee - lastMonthFee), useMore ? ConsoleColor.Red : ConsoleColor.Green));
     }
 
     private static string GetUsername(int line) {
@@ -48,7 +58,7 @@ class Program {
         List<FormatText> pairs = types
             .ToList()
             .ToDictionary(x => x, x => x == types[selected] ? ConsoleColor.Green : ConsoleColor.Red)
-            .Select(x => new FormatText(x.Key + " ", x.Value))
+            .Select(x => FormatText.Of(x.Key + " ", x.Value))
             .ToList();
         ConsoleUtils.WriteLineAt(line, "Type\t(Tab: Next, Enter: Select)");
         ConsoleUtils.WriteWithColorAt(line + 1, [.. pairs, FormatText.Of($"({types[selected].GetDescription()})", null)]);
@@ -73,14 +83,31 @@ class Program {
             type = types[selected];
             ConsoleUtils.WriteWithColorAt(line, FormatText.Of("Type: ", null), FormatText.Of(type, ConsoleColor.Green));
             ConsoleUtils.ClearLine(line + 1);
-        } catch (Exception e) {
+        } catch (Exception) {
             goto readInput;
         }
         return type;
     }
 
-    private static double GetWaterUse(int line, string message) {
+    private static double GetWaterUse(int line, string message, string successText) {
         double amount = 0.0;
+
+    input:
+        ConsoleUtils.WriteAt(line, message);
+        string? input = Console.ReadLine();
+        if (input == null) {
+            ConsoleUtils.WriteWithColorAt(line, FormatText.Of("Require input", ConsoleColor.Red));
+            Console.ReadKey();
+            goto input;
+        } else {
+            if (!double.TryParse(input, out amount)) {
+                ConsoleUtils.WriteWithColorAt(line, FormatText.Of("Invalid number", ConsoleColor.Red));
+                Console.ReadKey();
+                goto input;
+            } else {
+                ConsoleUtils.WriteWithColorAt(line, FormatText.Of(successText, null), FormatText.Of(amount, ConsoleColor.Green));
+            }
+        }
 
         return amount;
     }
@@ -93,7 +120,8 @@ class Program {
 enum CustomerType {
     Household,
     PublicService,
-    Production
+    Production,
+    Business
 }
 
 static class CustomerTypeDescription {
@@ -102,7 +130,88 @@ static class CustomerTypeDescription {
             CustomerType.Household => "Household customer",
             CustomerType.PublicService => "Administrative agency, public services",
             CustomerType.Production => "Production units",
+            CustomerType.Business => "Business service",
             _ => "Unknown",
         };
     }
+}
+
+static class CustomerFeeCalcMethod {
+    private static double EnviromentProtectionFees(double price) {
+        return price * 0.1;
+    }
+    private static double VAT(double fee) {
+        return fee * 0.1;
+    }
+
+    public static Func<double, double> GetCalcMethod(this CustomerType type) {
+        return type switch {
+            CustomerType.Household => (Func<double, double>) (amount => {
+                IList<double> price = ((double[]) [5973, 7052, 8699, 15929]).ToImmutableList();
+                IList<double> epf = price.Select(x => x * 0.1).ToImmutableList();
+
+                double[] group = new double[price.Count];
+
+                double fee = 0.0;
+
+                group[0] = Math.Min(amount, 10);
+                amount -= group[0];
+                group[1] = Math.Min(amount, 10);
+                amount -= group[1];
+                group[2] = Math.Min(amount, 10);
+                amount -= group[2];
+                group[3] = amount;
+                amount -= group[3];
+
+                if (amount == 0) {
+                    for (int i = 0; i < group.Length; i++) {
+                        double tempAmount = group[i];
+                        fee += tempAmount * price.ElementAt(i);
+                        fee += tempAmount * epf.ElementAt(i);
+                    }
+                }
+
+                fee += VAT(fee);
+
+                return fee;
+            }
+                ),
+            CustomerType.PublicService => (Func<double, double>) (amount => {
+                double price = 9955;
+                double fee = 0.0;
+                double epf = 995.5;
+
+                fee += price * amount;
+                fee += epf * amount;
+                fee += VAT(fee);
+
+                return fee;
+            }),
+            CustomerType.Production => (Func<double, double>) (amount => {
+                double price = 11615;
+                double fee = 0.0;
+                double epf = 1161.5;
+
+                fee += price * amount;
+                fee += epf * amount;
+                fee += VAT(fee);
+
+                return fee;
+            }),
+            CustomerType.Business => (Func<double, double>) (amount => {
+                double price = 22068;
+                double fee = 0.0;
+                double epf = 2206.8;
+
+                fee += price * amount;
+                fee += epf * amount;
+                fee += VAT(fee);
+
+                return fee;
+            }),
+            _ => throw new NotImplementedException()
+        };
+    }
+
+
 }
